27b110004fa268de6ee050e370193e63f273ee8a
varunchoudhary <varun.choudhary@limeroad.com>
2018-06-18 12:59:11 +0530

    toy story

--- ToyApp/app/models/micrpost.rb
+++ ToyApp/app/models/micrpost.rb
@@ -1,4 +1,4 @@
 class Micrpost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+  validates :content, length: { maximum: 140 },presence: true
 end
--- ToyApp/app/models/user.rb
+++ ToyApp/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
   has_many :micrposts
+  validates FILL_IN, presence: true
+  validates FILL_IN, presence: true
 end

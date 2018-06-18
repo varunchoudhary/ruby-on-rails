1680fec092af928552093afd0efc339f95fea247
varunchoudhary <varun.choudhary@limeroad.com>
2018-06-18 13:06:25 +0530

    added static pages controller

---
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get helps" do
+    get :helps
+    assert_response :success
+  end
+
+end

16daaf2ae840f2a20bf63308cd699c122e5cce4c
varunchoudhary <varun.choudhary@limeroad.com>
2018-06-20 14:55:45 +0530

    initialize helper

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -1,19 +1,26 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+  @base_title = "Ruby on Rails Tutorials Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+
   end
 
   test "should get helps" do
     get :helps
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
   test "should get about" do
     get :about
     assert_response :success
-    #assert_select "title", "About | Ruby on rails sample app"
+    assert_select "title", "About | #{@base_title}"
   end
 
 end

9c9c44cef719180d3a393d66e83bba616ca83d87
varunchoudhary <varun.choudhary@limeroad.com>
2018-06-20 11:25:37 +0530

    finish static pages

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -10,5 +10,10 @@ class StaticPagesControllerTest < ActionController::TestCase
     get :helps
     assert_response :success
   end
+  test "should get about" do
+    get :about
+    assert_response :success
+    #assert_select "title", "About | Ruby on rails sample app"
+  end
 
 end
